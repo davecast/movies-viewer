17fcb15e872efb5d3ca470b2d0f856eb73b8454d
David Castillo <hola@davecastworks.com>
2020-07-01 02:19:24 -0400

    Add functionality to closed button

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,19 @@
 const $movies = document.querySelector(".movies");
 const $movieAction = document.querySelectorAll(".movie__action");
+const $movieClose = document.querySelectorAll(".m-closed");
 
 
 function resetMovie() {
   let $openMovie = document.querySelector('.open');
   if ($openMovie !== null ) {
     $openMovie.classList.remove('open');
+    console.log('hola')
   }
 }
 
 $movieAction.forEach((element) => {
   element.addEventListener("click", () => {
-
+    console.log('hola2')
     resetMovie();
 
     if (!$movies.classList.contains('active')) {
@@ -20,4 +22,13 @@ $movieAction.forEach((element) => {
     
     element.parentElement.classList.add('open');
   });
+});
+
+$movieClose.forEach((element) => {
+  
+  element.addEventListener('click', (e) => {
+    e.stopPropagation();
+    resetMovie();
+    $movies.classList.remove('active');
+  })
 });
