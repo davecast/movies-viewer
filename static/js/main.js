9432d67e748f592cb1e23c0116f795b2d0d91ef6
David Castillo <hola@davecastworks.com>
2020-07-02 04:16:05 -0400

    Add content view and animations

--- static/js/main.js
+++ static/js/main.js
@@ -2,33 +2,34 @@ const $movies = document.querySelector(".movies");
 const $movieAction = document.querySelectorAll(".movie__action");
 const $movieClose = document.querySelectorAll(".m-closed");
 
-
 function resetMovie() {
-  let $openMovie = document.querySelector('.open');
-  if ($openMovie !== null ) {
-    $openMovie.classList.remove('open');
-    console.log('hola')
+  let $openMovie = document.querySelector(".open");
+  if ($openMovie !== null) {
+    $openMovie.classList.add("hidden");
+    $openMovie.classList.remove("open");
+    setTimeout(() => {
+      $openMovie.classList.remove("hidden");
+    }, 450);
   }
 }
 
 $movieAction.forEach((element) => {
   element.addEventListener("click", () => {
-    console.log('hola2')
+    console.log("hola2");
     resetMovie();
 
-    if (!$movies.classList.contains('active')) {
-      $movies.classList.add('active');
+    if (!$movies.classList.contains("active")) {
+      $movies.classList.add("active");
     }
-    
-    element.parentElement.classList.add('open');
+
+    element.parentElement.classList.add("open");
   });
 });
 
 $movieClose.forEach((element) => {
-  
-  element.addEventListener('click', (e) => {
+  element.addEventListener("click", (e) => {
     e.stopPropagation();
     resetMovie();
-    $movies.classList.remove('active');
-  })
+    $movies.classList.remove("active");
+  });
 });
