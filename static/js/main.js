86b877de389d41727f8c32ccb9dd036770d66769
David Castillo <hola@davecastworks.com>
2020-07-04 17:22:08 -0400

    Add functionality for video trailer

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 const $movies = document.querySelector(".movies");
 const $movieAction = document.querySelectorAll(".movie__action");
-const $movieClose = document.querySelectorAll(".m-closed");
+const $movieClose = document.querySelectorAll(".close__movie");
+let isOpen = false;
 
 function resetMovie() {
   let $openMovie = document.querySelector(".open");
@@ -14,15 +15,17 @@ function resetMovie() {
 }
 
 $movieAction.forEach((element) => {
-  element.addEventListener("click", () => {
-    console.log("hola2");
-    resetMovie();
+  element.addEventListener("click", (e) => {
+    if (!isOpen || !e.target.parentElement.classList.contains("open")) {
+      resetMovie();
 
-    if (!$movies.classList.contains("active")) {
-      $movies.classList.add("active");
+      if (!$movies.classList.contains("active")) {
+        $movies.classList.add("active");
+      }
+
+      element.parentElement.classList.add("open");
+      isOpen = true;
     }
-
-    element.parentElement.classList.add("open");
   });
 });
 
@@ -31,5 +34,66 @@ $movieClose.forEach((element) => {
     e.stopPropagation();
     resetMovie();
     $movies.classList.remove("active");
+    isOpen = false;
+  });
+});
+
+//document.getElementById("myVideo").src = "mov_bbb.ogg";
+
+/*VIDEO*/
+const $videoPlay = document.querySelectorAll(".view__video");
+const $video = document.getElementById("video");
+const $closeVideo = document.getElementById("close-video");
+
+$closeVideo.addEventListener("click", (e) => {
+  closeVideo(e.target.parentElement.querySelector("video"));
+});
+
+$videoPlay.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.dataset.video) {
+      console.log(e.target.dataset.video);
+      openVideo(e.target.dataset.video, e.target.dataset.poster);
+    } else {
+      console.dir(e.target.parentElement.dataset.video);
+      openVideo(
+        e.target.parentElement.dataset.video,
+        e.target.parentElement.dataset.poster
+      );
+    }
+  });
+});
+
+function openVideo(video, poster) {
+  $video.src = `./static/video/${video}`;
+  $video.play();
+
+  const $videoContainer = document.querySelector(".video__container");
+  $videoContainer.classList.add("show_video");
+
+  $movies.classList.add("show_video");
+  setTimeout(() => {
+    $videoContainer.classList.add("play");
+    $movies.classList.add("play");
+  }, 500);
+}
+
+function closeVideo(video) {
+  const $videoContainer = document.querySelector(".video__container");
+  video.pause();
+  video.currentTime = 0;
+  $videoContainer.classList.remove("show_video");
+  $movies.classList.remove("show_video");
+  setTimeout(() => {
+    $videoContainer.classList.remove("play");
+    $movies.classList.remove("play");
+  }, 500);
+}
+
+const $videos = document.querySelectorAll("video");
+
+$videos.forEach((video) => {
+  video.addEventListener("ended", () => {
+    closeVideo(video);
   });
 });
